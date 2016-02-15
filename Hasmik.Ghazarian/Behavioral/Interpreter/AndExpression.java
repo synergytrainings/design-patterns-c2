5e01db7e711b5e86569625014ab8aee7d2444a11
Hasmik Ghazaryan <hasmik.ghazaryan@arm.synisys.com>
2016-02-15 13:20:43 +0400

    Interpreter design pattern

---
+++ Hasmik.Ghazarian/Behavioral/Interpreter/AndExpression.java
@@ -0,0 +1,20 @@
+package interpreter;
+
+/**
+ * Author: Hasmik Ghazaryan on 2/15/2016.
+ */
+public class AndExpression implements Expression {
+
+    private Expression expr1;
+    private Expression expr2;
+
+    public AndExpression(Expression expr1, Expression expr2) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expr1.interpret(context) && expr2.interpret(context);
+    }
+}
---
+++ Hasmik.Ghazarian/Behavioral/Interpreter/Expression.java
@@ -0,0 +1,8 @@
+package interpreter;
+
+/**
+ * Author: Hasmik Ghazaryan on 2/15/2016.
+ */
+public interface Expression {
+    boolean interpret(String context);
+}
