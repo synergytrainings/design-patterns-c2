49c59117206aae2ee59d42a25ffe89af5235ebf4
David Shahbazyan <d.shahbazyan@gmail.com>
2016-03-03 03:40:25 +0400

    Mediator pattern implementation.

---
+++ david.shahbazyan/Mediator/src/Main.java
@@ -0,0 +1,25 @@
+import impl.Channel;
+import impl.User;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * <b>Author:</b> David Shahbazyan <br/>
+ * <b>Date:</b> 3/3/16 <br/>
+ * <b>Time:</b> 1:33 AM <br/>
+ */
+public class Main {
+    private static AtomicInteger ID_GENERATOR = new AtomicInteger(0);
+    public static void main(String[] args) {
+        Channel channel = new Channel();
+        for (int i = 0; i < 5; i++) {
+            channel.addUser(new User(channel, ID_GENERATOR.incrementAndGet(), "User_" + ID_GENERATOR.get()));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            int index = (int) (Math.random() * 5);
+            channel.getUsers().get(index).sendMessage(String.format("New message from USER%d", index + 1));
+            System.out.println();
+        }
+    }
+}
---
+++ david.shahbazyan/Mediator/src/impl/Channel.java
@@ -0,0 +1,39 @@
+package impl;
+
+import api.Client;
+import api.Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <b>Author:</b> David Shahbazyan <br/>
+ * <b>Date:</b> 3/3/16 <br/>
+ * <b>Time:</b> 1:44 AM <br/>
+ */
+public class Channel implements Mediator {
+    private List<Client> users = new ArrayList<>();
+
+    public void addUser(Client client) {
+        this.users.add(client);
+    }
+    public void removeUser(Client client) {
+        this.users.remove(client);
+    }
+
+    public List<Client> getUsers() {
+        return users;
+    }
+
+    @Override
+    public void sendMessage(String message, Client fromClient) {
+        String formattedMessage = String.format("%s says: %s", fromClient.getUserName(), message);
+        for (Client user : users) {
+            if (!user.equals(fromClient)) {
+                System.out.println(formattedMessage);
+                // Send message functionality goes here...
+                user.notifyAboutNewMessage();
+            }
+        }
+    }
+}
